ad1c70a6520cd6c5b3e3e939d2474638e7c5d9db
FireLemons <shenyang64@gmail.com>
2017-09-09 10:10:17 -0500

    Created data model class

---
+++ CS4750HW2/puzzle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4750HW2
+{
+    class Puzzle {
+
+        // Data model of puzzle
+        private int[,] puzzle = new int[3, 3];
+
+
+        public Puzzle(int[,] puzzle) {
+            this.puzzle = puzzle;
+        }
+
+
+        public Point[] getChildren(Point parent, Point node) {
+            return null;
+        }
+    }
+}
