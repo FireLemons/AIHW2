138c41267474d759fe95b78a9620539e68ddf1b2
Peter Eckelmann <p.eckelmann@gmail.com>
2017-09-09 11:14:12 -0500

    working on manahtan distance function

--- CS4750HW2/puzzle.cs
+++ CS4750HW2/puzzle.cs
@@ -7,19 +7,37 @@ using System.Threading.Tasks;
 
 namespace CS4750HW2
 {
-    class Puzzle {
+    class Puzzle
+    {
 
         // Data model of puzzle
         private int[,] puzzle = new int[3, 3];
 
 
-        public Puzzle(int[,] puzzle) {
+        public Puzzle(int[,] puzzle)
+        {
             this.puzzle = puzzle;
         }
 
 
-        public Point[] getChildren(Point parent, Point node) {
+        public Point[] getChildren(Point parent, Point node)
+        {
             return null;
-        }
-    }
-}
+        } 
+
+        public int getSingleManahatanDistance(Point node)
+        {
+            //Declare variables
+            int distance = -1;
+            int nodeNum = puzzle[node.X, node.Y];
+
+            switch(nodeNum)
+            {
+
+            } //End 
+
+
+            return distance;
+        } //End 
+    } //End class Puzzle
+} //End namespace CS4750HW2
