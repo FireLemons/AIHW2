41b2752ec7aa764b74390f2e9312de643762c98f
FireLemons <shenyang64@gmail.com>
2017-09-11 15:46:03 -0500

    DFS started

---
+++ CS4750HW2/DFS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4750HW2
+{
+    class DFS
+    {
+        private Puzzle puzzle;
+        private List<int[,]> closed;
+        private Stack<Direction> fringe;
+
+        public DFS(Puzzle puzzle){
+            this.puzzle = puzzle;//load initial state of problem
+        }
+
+        public void performDepthFirstGraphSearch()
+        {
+            if (puzzle.isInGoalState()) {
+
+            }
+        }
+    }
+}
